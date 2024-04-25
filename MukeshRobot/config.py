02a26377f0c7f9fbb6e256e2a0d1ea1ce0fdf81f
Kanaryu <162136822+SeiryuXD@users.noreply.github.com>
2024-04-26 03:02:47 +0700

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -6,15 +6,15 @@ class Config(object):
 
     API_ID = "27776767" # integer value, dont use ""
     API_HASH = "e7b0d8f7b037df9ff8b300816e90080b"
-    TOKEN = "6358179443:AAGvbnwaPtYNnYEur04yVENxH_mk7crE4bk"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    TOKEN = "6358179443:AAHL_kb4fkNXcrcg7W0j_P45xHIZ8y6pwCI"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 6975932205 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
-    SUPPORT_CHAT = "fananimeindo"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "FlameBloodsUnion"  # Your own group for support, do not add the @
     START_IMG = "https://telegra.ph/file/02c3abd53e810a92d2f20.jpg"
-    EVENT_LOGS = -1002007649771  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    EVENT_LOGS = -1002049125216  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     MONGO_DB_URI = "mongodb+srv://roundnessxyz:K8gfBy7UABqI0cRL@nahida-database.1yubfvl.mongodb.net/?retryWrites=true&w=majority&appName=Nahida-database"
     # RECOMMENDED
-    DATABASE_URL = "postgres://qjwlnsfz:e_OBB13W-zJodkUcrThSBmgRN0YiKdkj@flora.db.elephantsql.com/qjwlnsfz"  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgres://sdzssnho:yOsX_yr6nKBy4NeZWeI2hr8gmiUKsdOr@baasu.db.elephantsql.com/sdzssnho"  # A sql database url from elephantsql.com
     CASH_API_KEY = (
         "NKOM6OH1IVLTUA86"  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
